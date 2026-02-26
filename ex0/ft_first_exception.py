c62ab06bf3c45ce8ef00a4fdf8e2ada0f6367eea
Simon La Grange <sla-gran@fu-r4-p8.s19.be>
2026-02-26 11:36:01 +0100

    ex0

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+def check_temperature(temp_str) -> int:
+    print(f"Testing temperature: {temp_str}")
+    try:
+        temp_str = (int(temp_str))
+        if(temp_str >= 0 and temp_str <= 40):
+            print(f"Temperature {temp_str}°C is perfect for plants!\n")
+            return(temp_str)
+        elif (temp_str < 0):
+            print(f"Error: {temp_str}°C is too cold for plants (min 0°C)\n")
+        elif (temp_str > 40):
+            print(f"Error: {temp_str}°C is too hot for plants (max 40°C)\n")
+    except ValueError:
+        print(f"Error: {temp_str} is not a valid number\n")
+
+
+def test_temperature_input() -> None:
+    check_temperature("25")
+    check_temperature("abc")
+    check_temperature("100")
+    check_temperature("-50")
+    print("All tests completed - program didn't crash!")
+
+if __name__ == "__main__":
+    print("=== Garden Temperature Checker ===\n")
+    test_temperature_input()
--- ft_first_exception.py
+++
@@ -1,9 +0,0 @@
-def check_temperature(temp_str) -> int:
-    if not type(temp_str) is int:
-        raise TypeError(f"Error: '{temp_str}' is not a number")
-    elif temp_str > 40:
-        raise Exception(f"Error: {temp_str}°C is too hot for plants (max 40°C)")
-    elif temp_str < 0:
-        raise Exception(f"Error: {temp_str}°C is too cold for plants (min 0°C)")
-    else:
-        print(f"Temperature {temp_str}°C is perfect for plants!")
