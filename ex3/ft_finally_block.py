bda1ac3484c0fa885eb0bd754cd505f4134b4333
Simon La Grange <sla-gran@shi-r5-p1.s19.be>
2026-03-02 13:54:04 +0100

    debut ex3

---
+++ ex3/ft_finally_block.py
@@ -0,0 +1,9 @@
+def water_plants(plant_list) -> None:
+    print("Opening watering system!")
+    try:
+        pass
+    except Exception as e:
+        raise 
+    finally:
+        print("Closing watering system (cleanup)")
+
