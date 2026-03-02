d9bfaf50fa75368e7e238d317b7816f7911da27d
Simon La Grange <sla-gran@shi-r5-p1.s19.be>
2026-03-02 12:28:34 +0100

    ex2

--- ex2/ft_custom_errors.py
+++ ex2/ft_custom_errors.py
@@ -1,13 +1,18 @@
 class GardenError(Exception):
 
+    def __init__(self, message, errors) -> None:
+        super().__init__(message)
+        self.errors = errors
 
 
 class PlantError(GardenError):
-
+    def __init__(self, message, errors) -> None:
+        super().__init__(message, errors)
 
 
 class WaterError(GardenError):
-
+    def __init__(self, message, errors) -> None:
+        super().__init__(message, errors)
 
 
 """if Plant.days >= 50:
