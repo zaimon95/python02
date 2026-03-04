94cd325e9a3f19d6b90bb0c35c40fa26142a6f99
Simon La Grange <sla-gran@shi-r5-p1.s19.be>
2026-03-04 11:59:35 +0100

    debut ex3

--- ex3/ft_finally_block.py
+++ ex3/ft_finally_block.py
@@ -1,8 +1,8 @@
-def water_plants(plant_list: str) -> None:
+def water_plants(plant_list: list[str]) -> None:
     print("Opening watering system!")
     try:
         print("")
     except Exception as e:
-        print({e})
+        print(e)
     finally:
         print("Closing watering system (cleanup)\n")
